7c83a20d9bccedf0456604b41ed7d60307b532ec
viho-kernel <vihari.reddy1802@gmail.com>
2025-10-18 23:20:11 +0530

    another commit

--- terraform-file/ec2.tf
+++ terraform-file/ec2.tf
@@ -1,19 +1,21 @@
-resource "aws_instance" "demo-server" {
-    ami = "ami-0341d95f75f311023"
+resource "aws_instance" "project-server" {
+    ami = "ami-0360c520857e3138f"
     instance_type = "t2.micro"
     key_name = "project-pair"
-    security_groups = [aws_security_group.demo-sg.name]
-   # Replace with your actual key pair name
+    //security_groups = [aws_security_group.project-sg.name]
+    subnet_id = aws_subnet.project-subnet-01.id
+    vpc_security_group_ids = [aws_security_group.project-sg.id]
+    for_each = toset(["Jenkins-master", "build-worker", "Ansible"])
     tags = {
-        Name = "DemoServer"
-        Environment = "Development"
+        Name = "${each.key}"
     }
   
 }
 
-resource "aws_security_group" "demo-sg" {
-    name        = "demo-security-group"
-    description = "Security group for demo server"
+resource "aws_security_group" "project-sg" {
+    name        = "project-security-group"
+    description = "Security group for project server"
+    vpc_id      = aws_vpc.project-vpc.id
 
     ingress {
         description = "Allow SSH access"
@@ -30,4 +32,67 @@ resource "aws_security_group" "demo-sg" {
         cidr_blocks = ["0.0.0.0/0"]
     }
 
+}
+
+resource "aws_vpc" "project-vpc" {
+  cidr_block = "10.1.0.0/16"
+    tags = {
+        Name = "ProjectVPC"
+        Environment = "Development"
+    }  
+
+}
+
+resource "aws_subnet" "project-subnet-01" {
+  vpc_id            = aws_vpc.project-vpc.id
+  cidr_block        = "10.1.1.0/24"
+  map_public_ip_on_launch = "true"
+  availability_zone = "us-east-1a"
+    tags = {
+        Name = "ProjectSubnet-01"
+        Environment = "Development"
+    }
+}
+
+resource "aws_subnet" "project-subnet-02" {
+  vpc_id            = aws_vpc.project-vpc.id
+  cidr_block        = "10.1.2.0/24"
+  map_public_ip_on_launch = "true"
+  availability_zone = "us-east-1a"
+    tags = {
+        Name = "ProjectSubnet-02"
+        Environment = "Development"
+    }
+}
+
+resource "aws_internet_gateway" "project-igw" {
+  vpc_id = aws_vpc.project-vpc.id
+    tags = {
+        Name = "ProjectIGW"
+        Environment = "Development"
+    }   
+  
+}
+
+resource "aws_route_table" "project-route-table" {
+  vpc_id = aws_vpc.project-vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.project-igw.id
+  }
+    tags = {
+        Name = "ProjectRouteTable"
+        Environment = "Development"
+    }
+  
+}
+
+resource "aws_route_table_association" "project-route-table-assoc-01" {
+  subnet_id      = aws_subnet.project-subnet-01.id
+  route_table_id = aws_route_table.project-route-table.id
+}
+
+resource "aws_route_table_association" "project-route-table-assoc-02" {
+  subnet_id      = aws_subnet.project-subnet-02.id
+  route_table_id = aws_route_table.project-route-table.id
 }
